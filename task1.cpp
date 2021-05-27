99c62b4f01d665b624e3283f2eda1850302fa2b4
hecate <hecateenitharmon@gmail.com>
2021-05-27 20:03:03 +0300

    Add comments and refactoring

--- task1.cpp
+++ task1.cpp
@@ -3,25 +3,30 @@
 #include <regex>
 using namespace std;
 
+// объявление строковых переменных с сохранением значений выводимого в консоль текста
 string z1Text = "The value of z1 = ";
 string z2Text = "The value of z2 = ";
 string enterText = "Enter Alfa between or equal to 0 and 360";
 string exitText = "Do you want to continue? (1 for Yes, any key for No)";
 
+// функция подсчитывающая и возвращающая значение z1, принимает вводимый из консоли параметр alfa
 double countZ1(double alfa)
 {
     double z1 = (1 - 2 * pow(sin(alfa), 2)) / (1 + sin(2 * alfa));
     return z1;
 }
 
+// функция подсчитывающая и возвращающая значение z2, принимает вводимый из консоли параметр alfa
 double countZ2(double alfa)
 {
     double z2 = (1 - tan(alfa)) / (1 + tan(alfa));
     return z2;
 }
 
+// функция принимающая строковое значение, проверяющее может ли оно являться числовым значением, возвращает булиновое значение
 bool isNumber(string x)
 {
+    // регексп на определение являются ли все символы полученной строки цифрами
     regex e ("^-?\\d+");
     if (regex_match (x,e)) {
         return true;
@@ -29,30 +34,47 @@ bool isNumber(string x)
     return false;
 }
 
+// функция калькуляции и вывода
 int countLinearAlogoritm()
 {
+    // объявление переменной для сохранение числового значения
     int alfa;
+    // объявление переменной для введения строкового значения
     string input;
+    // выведение начального текста с информацией о вводимых параметрах
     cout << enterText << endl;
+    // сохранение вводимого значения в переменную
     cin >> input;
+    // проверка введенного значения, приведение к числовому типу и сохранение значения (или сохранение отрицательного значения, если были введены символы или буквы)
     alfa = isNumber(input) ? stoi(input) : -1;
+    // проверка диапазона вводимых значений
     if (alfa >= 0 && alfa <= 360)
     {
+        // сохранения результатом вычислений
         double z1 = countZ1(alfa);
         double z2 = countZ2(alfa);
+        // вывод результатов вычислений в консоль
         cout << z1Text << z1 << endl << z2Text << z2 << endl;
 
-        bool answer;
+        // объявление переменной
+        bool isContinue;
+        // вывод текста возможного завершения программы
         cout << exitText << endl;
-        cin >> answer;
-        if (answer) {
+        // сохранение вводимого значения из консоли в переменную
+        cin >> isContinue;
+        // проверка введенного значения
+        if (isContinue) {
+            // повторный вызов программы
             countLinearAlogoritm();
         }
+        // завершение программы
         return 0;
     }
+    // повторный вызов программы при введении невалидного значения
     countLinearAlogoritm();
 }
 
+// основная функция выполнения программы
 int main() 
 {
     countLinearAlogoritm();
--- task2.cpp
+++ task2.cpp
@@ -3,14 +3,17 @@
 #include <regex>
 using namespace std;
 
+// объявление строковых переменных с сохранением значений выводимого в консоль текста
 string enterText = "Enter Z number: ";
 string exitText = "Do you want to continue? (1 for Yes, any key for No)";
 string resultText = "Function result = ";
 string positiveText = "x > 0 The Result: ";
 string negativeText = "x <= 0 The Result: ";
 
+// функция принимающая строковое значение, проверяющее может ли оно являться числовым значением, возвращает булиновое значение
 bool isNumber(string x)
 {
+    // регексп на определение являются ли все символы полученной строки цифрами
     regex e ("^-?\\d+");
     if (regex_match (x,e)) {
         return true;
@@ -18,49 +21,72 @@ bool isNumber(string x)
     return false;
 }
 
+// функция вычисления и возвращения значения х, принимающая аргумент z
 double countX(double z)
 {
     double x;
+    // вычисление при z > 0
     if (z > 0) {
         x = 1 / (pow(z, 2) + 2 * z);
+        // вывод сообщения из ветви z > 0
         cout << positiveText << x << endl;
         return x;
         
     }
+    // вычисление при z <= 0
     x = 1 - pow(z, 3);
+    // вывод сообщения из ветви z <= 0
     cout << negativeText << x << endl;
     return x;
 }
 
+// функция вычисления и врзвращения значения y, принимающая аргумент x
 double countY(double x) 
 {
     double y = (2 * pow(M_E, -3 * x) - 4 * pow(x, 2)) / (log(abs(x)) + x);
     return y;
 }
 
+// функция подсчета
 int countFunction()
 {
+    // объявление переменной для числового значения
     int z;
+    // объявление переменной для введения строкового значения
     string input;
+    // выведение начального текста с информацией о вводимых параметрах
     cout << enterText << endl;
+    // сохранение вводимого значения в переменную
     cin >> input;
+    // проверка значения, является ли он числом
     if (isNumber(input)) {
+        // преобразование и сохранение числового значения
         z = stoi(input);
+        // сохранения результатов вычислений
         double x = countX(z);
         double y = countY(x);
+        // вывод результатов вычислений в консоль
         cout << resultText << y << endl;
 
+        // объявление переменной
         bool isContinue;
+        // вывод текста возможного завершения программы
         cout << exitText << endl;
+        // сохранение вводимого значения из консоли в переменную
         cin >> isContinue;
+        // проверка введенного значения
         if (isContinue) {
+            // повторный вызов программы
             countFunction();
         }
+        // завершение программы
         return 0;
     }
+    // повторный вызов программы при введении невалидного значения
     countFunction();
 }
 
+// основная функция выполнения программы
 int main() 
 {
     countFunction();
